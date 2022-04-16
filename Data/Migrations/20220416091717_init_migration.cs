65662a5bc6a2b45183a261ef6078f93ecab1c1a2
ncumplido <ncumplido@msn.com>
2022-04-16 10:36:16 +0100

    init commit

---
+++ Data/Migrations/20220416091717_init_migration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NichDevDotNetCore.Data.Migrations
+{
+    public partial class init_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NichDevDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
